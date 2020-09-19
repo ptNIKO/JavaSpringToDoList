9770641ad2d44578cf9e8b8d91746fe07d3f40ed
ptNIKO <simonich2016@gmail.com>
2020-09-19 12:35:55 +0300

    First commit app

---
+++ src/main/java/com/todolist/springtodolist/JavaToDoListSpringApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToDoListSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToDoListSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/springtodolist/JavaToDoListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.springtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToDoListSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
