a4ef46e265444ff9c6bfa0e3188cdc09b81ce952
ptNIKO <simonich2016@gmail.com>
2020-10-08 07:10:37 +0300

    add function delete task

--- src/main/java/com/todolist/springtodolist/MainController.java
+++ src/main/java/com/todolist/springtodolist/MainController.java
@@ -38,21 +38,57 @@ public class MainController {
         return "main";
     }
 
+    @PostMapping("separat")
+    public String separationMethod(@RequestParam(value = "checkboxName", required = false) String checkboxValue,
+                                 @ModelAttribute("idTask") String idTask,
+                                 @ModelAttribute("deleteButton") String deleteButton,
+                                 Model model) {
+
+        if (deleteButton.equals("deleteButton"))
+            deleteTask(idTask, model);
+        else if (deleteButton.equals("finishUpButton"))
+            finishUpTask(checkboxValue, idTask, model);
+        else
+            submitTask(checkboxValue, idTask, model);
+
+
+        return "main";
+    }
+
     @PostMapping("submitTask")
-    public String submitTask(@RequestParam(value = "checkboxName", required = false) String checkboxValue,
-                             @ModelAttribute("idTask") String idTask,
+    public String submitTask(String checkboxValue,
+                             String idTask,
                              Model model) {
 
-        if(checkboxValue != null) {
-            System.out.println("checkbox is checked");
-
-        }
-        else {
-            System.out.println("checkbox is not checked");
-        }
 
         Iterable<Task> tasks = taskRepo.findAll();
         Task task = taskRepo.findById(Integer.parseInt(idTask)).get();
+
+            if (checkboxValue != null && taskRepo.findById(Integer.parseInt(idTask)).get().isStatus()){
+                task.setStatus(false);
+                taskRepo.save(task);
+            }
+            else{
+                task.setStatus(true);
+                taskRepo.save(task);
+            }
+
+            model.addAttribute("tasks", tasks);
+            System.out.println(idTask);
+
+
+        return "main";
+    }
+
+    @PostMapping("finishUpTask")
+    public String finishUpTask(String checkboxValue,
+                             String idTask,
+                             Model model) {
+
+
+        Iterable<Task> tasks = taskRepo.findAll();
+        Task task = taskRepo.findById(Integer.parseInt(idTask)).get();
+
 
         if (checkboxValue != null && taskRepo.findById(Integer.parseInt(idTask)).get().isStatus()){
             task.setStatus(false);
@@ -65,6 +101,19 @@ public class MainController {
 
         model.addAttribute("tasks", tasks);
         System.out.println(idTask);
+
+
+        return "main";
+    }
+
+    @PostMapping("deleteTask")
+    public String deleteTask(String idTask,
+                             Model model) {
+
+        taskRepo.deleteById(Integer.parseInt(idTask));
+        Iterable<Task> tasks = taskRepo.findAll();
+        model.addAttribute("tasks", tasks);
+        //System.out.println("Delete: " + task.getName_task());
         return "main";
     }
 }
